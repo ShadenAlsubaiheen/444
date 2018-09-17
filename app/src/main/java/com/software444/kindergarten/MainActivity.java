390e58a037c54067472612684352c360b9ff7c49
shaden <shaden.51@hotmail.com>
2018-09-17 09:45:06 +0300

    hello this is first commit

--- app/src/main/java/com/software444/kindergarten/MainActivity.java
+++ app/src/main/java/com/software444/kindergarten/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //shaden aaaa
+    //reemaaaa fofo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
